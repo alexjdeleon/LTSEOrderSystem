3fae23de14097ac3c0f62023baa3a363b5129f40
Alex DeLeon <alexjdeleonm@gmail.com>
2020-07-03 15:53:23 -0400

    Modified *nix build script to create necessary directory

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 find . -name "*.java" ! -name "*Test.java" > sources.txt
+mkdir bin
 javac -cp lib/* -d bin @sources.txt
 jar cfm LTSEOrderSystem.jar manifest.txt -C lib . -C src/main/resources . -C bin .
+rm -r bin
