e5a251e4c70fbdd70d5d00bfcc26bd81455ba001
Alex DeLeon <alexjdeleonm@gmail.com>
2020-07-03 21:56:55 -0400

    Updated JUnit test cases to test all rejection scenarios

--- src/test/java/com/ltse/orders/test/LTSEOrderProcessorTest.java
+++ src/test/java/com/ltse/orders/test/LTSEOrderProcessorTest.java
@@ -3,6 +3,8 @@ package com.ltse.orders.test;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,13 +23,39 @@ import com.ltse.orders.exceptions.PropertiesInitializatonException;
 class LTSEOrderProcessorTest {
 
 	private LTSEOrderProcessor orderProcessor;
-
+	private String testDataDirectory = "src/main/resources/test_data/";
+	
+	@Test
+	void testMultipleRejectedOrders() throws PropertiesInitializatonException, IOException {
+		
+		String testFileName = testDataDirectory + "multiple_orders_rejected_in.csv";
+		
+		orderProcessor = new LTSEOrderProcessor(testFileName);
+		orderProcessor.processTradeOrders();
+		
+		// Compare accepted order files
+		String expectedAcceptedOutputFileName = "/test_data/multiple_orders_accepted_out.csv";
+		String acceptedOutputFileName = "output/accepted_trades.csv";
+		if (!filesAreEqual(expectedAcceptedOutputFileName, acceptedOutputFileName)) {
+			fail("The output file does not match the reference data.");
+		}
+		
+		// Compare rejected order files
+		String expectedRejectedOutputFileName = "/test_data/multiple_orders_rejected_out.csv";
+		String rejectedOutputFileName = "output/rejected_trades.csv";
+		if (!filesAreEqual(expectedRejectedOutputFileName, rejectedOutputFileName)) {
+			fail("The output file does not match the reference data.");
+		}
+	}
+	
 	@Test
 	void testAllOrdersAccepted() throws PropertiesInitializatonException, IOException {
-		String testFileName = "/test_data/all_orders_accepted_input.csv";
-		String expectedOutputFileName = "/test_data/all_orders_accepted_output.csv";
-		String outputFileName = "/accepted_trades.csv";
 		
+		String testFileName = testDataDirectory + "all_orders_accepted_in.csv";
+		
+		String expectedOutputFileName = "/test_data/all_orders_accepted_out.csv";
+		String outputFileName = "output/accepted_trades.csv";
+
 		orderProcessor = new LTSEOrderProcessor(testFileName);
 		orderProcessor.processTradeOrders();
 
@@ -36,21 +64,51 @@ class LTSEOrderProcessorTest {
 		}
 	}
 	
-	
-	boolean filesAreEqual(String testFile, String outputFile) throws IOException {
+	@Test
+	void testValidJSON() throws PropertiesInitializatonException, IOException {
+		
+		String testFileName = testDataDirectory + "all_orders_accepted_in.csv";
+		
+		String expectedOutputFileName = "/test_data/valid_json_file.json";
+		String outputFileName = "output/accepted_trades.json";
+
+		orderProcessor = new LTSEOrderProcessor(testFileName);
+		orderProcessor.processTradeOrders();
+
+		if (!filesAreEqual(expectedOutputFileName, outputFileName)) {
+			fail("The output file does not match the reference data.");
+		}
+	}
+
+	/**
+	 * Compares the two files line by line and returns true if the contents are
+	 * equal
+	 * 
+	 * @param testFile
+	 * @param outputFileName
+	 * @return
+	 * @throws IOException
+	 */
+	boolean filesAreEqual(String testFile, String outputFileName) throws IOException {
 
 		InputStream testFileInputStream = LTSEOrderProcessorTest.class.getResourceAsStream(testFile);
-		BufferedReader testFileReader = new BufferedReader(new InputStreamReader(testFileInputStream));
+		File outputFile = new File(outputFileName);
+		InputStream outputFileInputStream = new FileInputStream(outputFile);
 
-		InputStream outputFileInputStream = LTSEOrderProcessorTest.class.getResourceAsStream(outputFile);
-		BufferedReader outputFileReader = new BufferedReader(new InputStreamReader(outputFileInputStream));
+		try (BufferedReader outputFileReader = new BufferedReader(new InputStreamReader(outputFileInputStream));
+				BufferedReader testFileReader = new BufferedReader(new InputStreamReader(testFileInputStream))) {
 
-		String line;
-		while ((line = testFileReader.readLine()) != null) {
-			if (line != outputFileReader.readLine()) {
-				return false;
+			String testFileLine = testFileReader.readLine();
+			String outputFileLine = outputFileReader.readLine();
+			while (testFileLine != null) {
+				if (!testFileLine.equals(outputFileLine)) {
+					return false;
+				}
+				testFileLine = testFileReader.readLine();
+				outputFileLine = outputFileReader.readLine();
 			}
 		}
+
 		return true;
 	}
 
